1923e681c79b9db110ed5e0f86d8413102634a9d
alli-Codes <allisonevans081@gmail.com>
2023-10-16 23:04:45 +0100

    Getting user sign up details

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,18 +1,35 @@
+"use client";
 import Link from "next/link";
+import { useRef } from "react";
 import Button from "@/components/Button";
 
 export default function Signup() {
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
+
+  const getUserDetails = function () {
+    let email = emailInput.current.value;
+    let password = passwordInput.current.value;
+
+    console.log(email, password);
+  };
+
   return (
     <div className="h-screen flex flex-col items-stretch justify-center">
-      <form className="flex flex-col items-center px-4 gap-4">
+      <form
+        className="flex flex-col items-center px-4 gap-4"
+        onChange={getUserDetails}
+      >
         <input
           type="email"
           placeholder="Email"
+          ref={emailInput}
           className="bg-white w-full p-4 border border-1 active:outline outline-black"
         />
         <input
           type="password"
           placeholder="Password"
+          ref={passwordInput}
           className="bg-white w-full p-4 border border-1 active:outline outline-black"
         />
 
---
+++ helper/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBaoNrbv4LseDaYiNbGCEEPPTGhaMOGOok",
+  authDomain: "anonymous-texts-6f7ad.firebaseapp.com",
+  projectId: "anonymous-texts-6f7ad",
+  storageBucket: "anonymous-texts-6f7ad.appspot.com",
+  messagingSenderId: "184414442175",
+  appId: "1:184414442175:web:da85cb595b415e7edaec0a",
+  measurementId: "G-5D4KVZVNJW",
+};
